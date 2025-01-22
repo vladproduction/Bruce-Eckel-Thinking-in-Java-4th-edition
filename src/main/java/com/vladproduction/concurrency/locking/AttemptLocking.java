f434dbd6c271210b3e87706ab51b13e8221c15a8
vladproduction <vladbogdantsev@gmail.com>
2025-01-22 19:32:32 +0200

    ReentrantLock and Volatile examples;

---
+++ src/main/java/com/vladproduction/concurrency/locking/AttemptLocking.java
@@ -0,0 +1,77 @@
+package com.vladproduction.concurrency.locking;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Reentrant Locks:
+ * - The class uses a ReentrantLock, which is a concrete implementation of the Lock interface.
+ *   It allows threads to lock and unlock resources, providing finer control over concurrency compared to the synchronized keyword.
+ * - A reentrant lock allows the same thread to acquire the lock multiple times without causing a deadlock.
+ * */
+public class AttemptLocking {
+
+    private ReentrantLock lock = new ReentrantLock();
+    private CountDownLatch latch = new CountDownLatch(1);
+
+    //demonstrate non-blocking attempts to acquire a lock
+    //useful for avoiding deadlock scenarios where a thread might be blocked indefinitely while waiting for a lock
+    public void untimed(){
+        boolean captured = lock.tryLock(); //will return immediately, indicating whether the lock was captured or not
+        try{
+            System.out.println("tryLock(): " + captured);
+        }finally {
+            if(captured){
+                lock.unlock();
+            }
+        }
+    }
+
+    public void timed(){
+        boolean captured = false;
+        try{
+            captured = lock.tryLock(2, TimeUnit.SECONDS); //will wait for the specified time before giving up
+        }catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
+        try{
+            System.out.println("tryLock(2, TimeUnit.SECONDS): " + captured);
+        }finally {
+            if(captured){
+                lock.unlock();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        final AttemptLocking al = new AttemptLocking();
+        al.untimed();
+        al.timed();
+
+        new Thread(){
+            {
+                setDaemon(true);
+            }
+            public void run() {
+                al.lock.lock();
+                System.out.println("acquired");
+                al.latch.countDown(); // Signal that the lock is acquired
+            }
+        }.start();
+
+        try {
+            al.latch.await(); // Wait for the signal from the daemon thread
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        al.untimed();
+        al.timed();
+    }
+
+}
+/*The example shows how to create a new thread and indicates it as a daemon thread.
+Daemon threads are background threads that do not prevent the JVM from exiting when the program finishes,
+as they are terminated when all user threads (non-daemon threads) finish executing.*/
---
+++ src/main/java/com/vladproduction/concurrency/volatile_example/VolatileExample.java
@@ -0,0 +1,53 @@
+package com.vladproduction.concurrency.volatile_example;
+
+/**
+ * Visibility: When one thread modifies a volatile variable, that change is immediately visible to other threads.
+ * This means that if a thread writes to a volatile variable, any subsequent reads of that variable will see the updated value,
+ * even if those reads occur in different threads.
+ *
+ * No Caching: volatile variables are not cached in CPU registers. They are always read from and written to the main memory.
+ * This helps avoid stale data being read from local caches used by threads.
+ *
+ * No Atomicity: It is important to note that volatile only guarantees visibility; it does not ensure atomicity.
+ * If you need to perform compound actions (like check-then-act), you still need to use synchronized blocks or locks.
+ *
+ * More complex scenarios involving multiple operations, synchronization may still be necessary.
+ * */
+public class VolatileExample {
+
+    private static volatile boolean running = true; // Declaring a volatile variable
+
+    public static void main(String[] args) throws InterruptedException {
+
+        // Create a thread that will stop after some time
+        Thread worker = new Thread(() -> {
+            System.out.println("Worker thread started.");
+            while (running) {
+                // Simulating some work
+                // You could do something useful here
+                for (int i = 1; i <= 5; i++) {
+                    System.out.print(i + " ");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+            System.out.println("Worker thread stopped.");
+        });
+
+        worker.start();
+
+        // Main thread will let the worker run for a short time
+        Thread.sleep(5000); // Wait for 5 second
+
+        // Change the state of running to false
+        running = false; //main thread changes the value of running, the worker thread will see the updated value immediately
+
+        // Wait for the worker thread to finish
+        worker.join();
+
+        System.out.println("Main thread finished.");
+    }
+}
